2bed2cbb6919235764f4bd9fe569d80c1a2ac706
akashShanmugraj <akashshanmugaraj@gmail.com>
2023-02-13 18:01:39 +0530

    Added grid elements without background

--- src/pages/Clubs.js
+++ src/pages/Clubs.js
@@ -24,24 +24,15 @@ const Clubs = () => {
       <h1 className="text-4xl uppercase text-center mt-16">
         Our <span className="font-bold">Clubs</span>
       </h1>
-      <div className="grid grid-cols-2 lg:grid-cols-6 gap-8 mt-8">
-        {clubs.map((club) => (
-          <div className="w-full">
-            <div className="w-full aspect-square border-gray-600 border p-8">
-              <div
-                className="w-full h-full hover:scale-110 transition-all duration-300"
-                style={{
-                  background: `url(${club.image_url})`,
-                  backgroundSize: "contain",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "no-repeat",
-                }}
-              />
-            </div>
-            <p className="text-lg font-semibold font-sans mt-4">{club.clubName}</p>
-          </div>
-        ))}
-      </div>
+        <div className={'grid grid-cols-4 gap-10 '}>
+            <div className={'w-130 h-80 border-2 rounded-2xl font-bold bg-[#ABCDEF]'}>Column1</div>
+            <div className={'w-130 h-80 border-2 rounded-2xl font-bold bg-[#ABCDEF]'}>Column2</div>
+            <div className={'w-130 h-80 border-2 rounded-2xl font-bold bg-[#ABCDEF]'}>Column3</div>
+            <div className={'w-130 h-80 border-2 rounded-2xl font-bold bg-[#ABCDEF]'}>Column4</div>
+            <div className={'w-130 h-80 border-2 rounded-2xl font-bold bg-[#ABCDEF]'}>Column5</div>
+            <div className={'w-130 h-80 border-2 rounded-2xl font-bold bg-[#ABCDEF]'}>Column6</div>
+
+        </div>
     </Layout>
   );
 };
