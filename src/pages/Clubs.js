e511068eae2a75057d6bc0d08e79d37ddc06b1d6
akashShanmugraj <akashshanmugaraj@gmail.com>
2023-02-14 15:15:26 +0530

    Added Components

--- src/pages/Clubs.js
+++ src/pages/Clubs.js
@@ -5,6 +5,21 @@ import { Link } from "react-router-dom";
 import Layout from "./Layout.js";
 import { fetchAllClubsLogo } from "../API/calls.js";
 
+function ClubListing(props) {
+    return (
+        <a href={props.clubLink}>
+            <div className={'max-w-sm bg-white border border-gray-200 rounded-3xl shadow dark:bg-white dark:border-gray-700 overflow-hidden'}>
+                        <img className={"rounded-t-lg"} src={props.clubImage}/>
+                    <div className={"p-2"}>
+                        <h5 className="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-900">{props.clubName}</h5>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"><i>{props.clubDescription}</i></p>
+                    </div>
+            </div>
+        </a>
+
+    )
+}
+
 const Clubs = () => {
   const [clubs, setClubs] = useState([]);
 
@@ -24,13 +39,14 @@ const Clubs = () => {
       <h1 className="text-4xl uppercase text-center mt-16">
         Our <span className="font-bold">Clubs</span>
       </h1>
-        <div className={'grid grid-cols-4 gap-10 '}>
-            <div className={'w-130 h-80 border-2 rounded-2xl font-bold bg-[#ABCDEF]'}>Column1</div>
-            <div className={'w-130 h-80 border-2 rounded-2xl font-bold bg-[#ABCDEF]'}>Column2</div>
-            <div className={'w-130 h-80 border-2 rounded-2xl font-bold bg-[#ABCDEF]'}>Column3</div>
-            <div className={'w-130 h-80 border-2 rounded-2xl font-bold bg-[#ABCDEF]'}>Column4</div>
-            <div className={'w-130 h-80 border-2 rounded-2xl font-bold bg-[#ABCDEF]'}>Column5</div>
-            <div className={'w-130 h-80 border-2 rounded-2xl font-bold bg-[#ABCDEF]'}>Column6</div>
+        <div className={'grid grid-cols-4 gap-10 py-10'}>
+
+            <ClubListing clubName = "Book Readers Club" clubDescription = "Smells like paper, right outta press" clubImage = "https://images.unsplash.com/photo-1577985051167-0d49eec21977?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2978&q=80" clubLink = "https://www.facebook.com/psgtechbrc" />
+            <ClubListing clubName = "Book Readers Club" clubDescription = "Smells like paper, right outta press" clubImage = "https://images.unsplash.com/photo-1577985051167-0d49eec21977?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2978&q=80" clubLink = "https://www.facebook.com/psgtechbrc" />
+            <ClubListing clubName = "Book Readers Club" clubDescription = "Smells like paper, right outta press" clubImage = "https://images.unsplash.com/photo-1577985051167-0d49eec21977?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2978&q=80" clubLink = "https://www.facebook.com/psgtechbrc" />
+            <ClubListing clubName = "Book Readers Club" clubDescription = "Smells like paper, right outta press" clubImage = "https://images.unsplash.com/photo-1577985051167-0d49eec21977?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2978&q=80" clubLink = "https://www.facebook.com/psgtechbrc" />
+            <ClubListing clubName = "Book Readers Club" clubDescription = "Smells like paper, right outta press" clubImage = "https://images.unsplash.com/photo-1577985051167-0d49eec21977?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2978&q=80" clubLink = "https://www.facebook.com/psgtechbrc" />
+            <ClubListing clubName = "Book Readers Club" clubDescription = "Smells like paper, right outta press" clubImage = "https://images.unsplash.com/photo-1577985051167-0d49eec21977?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2978&q=80" clubLink = "https://www.facebook.com/psgtechbrc" />
 
         </div>
     </Layout>
