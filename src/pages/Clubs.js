246d5292931d98bb69a0e19dbe40f3fbe5789f64
Akash Shanmugaraj <65720968+akashShanmugraj@users.noreply.github.com>
2023-03-08 08:00:44 +0530

    Added model wrapper for Clubs

--- src/pages/Clubs.js
+++ src/pages/Clubs.js
@@ -3,45 +3,64 @@ import React, { useEffect, useState } from "react";
 import { TbExternalLink } from "react-icons/tb";
 import { Link } from "react-router-dom";
 import Layout from "./Layout.js";
-import { fetchAllClubsLogo } from "../API/calls.js";
+import { fetchClubDescription } from "../API/calls.js";
+//? import PersonList from "./ClubDescription.js";
+
+function ClubListing(props) {
+    return (
+            <a href={props.clubLink}>
+              <div className={'max-w-sm bg-white border-2 border-black rounded-3xl overflow-hidden'}>
+                  <a>
+                      <img className={"rounded-t-lg"} src={props.clubImage}/>
+                  </a>
+                  <div className={"p-5"}>
+                      <h5 className="mb-2 text-4xl font-bold tracking-tight text-gray-900">{props.clubName}</h5>
+                      <p class="mb-3 font-normal text-gray-700"><i>{props.clubTagLine}</i></p>
+                  </div>
+              </div>
+            </a>
+    )
+}
+
 
 const Clubs = () => {
-  const [clubs, setClubs] = useState([]);
+  const [clubs, setClubs] = useState({});
 
   useEffect(() => {
-    fetchAllClubsLogo()
+    fetchClubDescription()
       .then((res) => {
-        console.log(res.data);
-        setClubs(res.data);
+        let clubData = res.data
+        setClubs(clubData)
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
+// * console.log(clubs)
+// * ⬆️ works
+
+// todo Render Card component directly from API Fetch
+// todo Exclude ADMIN at 0 index while rendering 
+
+// ? API call does not have image link or clubDescription/clubTagLine
+// ? All clubImages (if present) would not be in same dimensions and would cause misproportionate  
+// ? Considering adding "category" field inside the card, with or without replacing TagLine
+
   return (
     <Layout>
       <h1 className="text-4xl uppercase text-center mt-16">
         Our <span className="font-bold">Clubs</span>
       </h1>
-      <div className="grid grid-cols-2 lg:grid-cols-6 gap-8 mt-8">
-        {clubs.map((club) => (
-          <div className="w-full">
-            <div className="w-full aspect-square border-gray-600 border p-8">
-              <div
-                className="w-full h-full hover:scale-110 transition-all duration-300"
-                style={{
-                  background: `url(${club.image_url})`,
-                  backgroundSize: "contain",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "no-repeat",
-                }}
-              />
-            </div>
-            <p className="text-lg font-semibold font-sans mt-4">{club.clubName}</p>
-          </div>
-        ))}
-      </div>
+        <div className={'grid grid-cols-4 gap-10 py-10'}>
+            <ClubListing clubName = "BRC Club" clubTagLine = 'Smells like paper, right outta press' clubImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Mercado_de_Col%C3%B3n%2C_Valencia%2C_Espa%C3%B1a%2C_2014-06-29%2C_DD_07.JPG/750px-Mercado_de_Col%C3%B3n%2C_Valencia%2C_Espa%C3%B1a%2C_2014-06-29%2C_DD_07.JPG"/>
+            {/* <ClubListing sampleClubData/> */}
+            <ClubListing />
+            <ClubListing />
+            <ClubListing />
+            <ClubListing />
+
+        </div>
     </Layout>
   );
 };
