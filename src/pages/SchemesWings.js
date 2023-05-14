f2ab451150648b1c32e89567ddddb17941553c59
jeyam03 <64417749+jeyam03@users.noreply.github.com>
2023-05-15 02:26:11 +0530

    Schemes Mobile

--- src/pages/SchemesWings.js
+++ src/pages/SchemesWings.js
@@ -20,38 +20,33 @@ const SchemesWings = () => {
       <h1 className="text-4xl uppercase text-center mt-16">
         Schemes & <span className="font-bold">Wings</span>
       </h1>
-      <div className={'flex flex-wrap justify-center gap-12 px-12 lg:px-0 py-12'}>
-        <div className="w-full bg-gray-200 rounded-2xl flex flex-row py-12 items-center shadow-md">
+      <div className={'flex flex-wrap justify-center gap-12 px-6 lg:px-0 py-12'}>
+        <div className="w-full bg-gray-200 rounded-2xl flex flex-col lg:flex-row py-12 px-6 items-center shadow-md gap-12 lg:gap-6">
 
-          <div className="w-1/2 flex flex-col items-center gap-12">
-            <img className="h-44 w-44" src="/assets/Schemes/nsslogo.png" alt="nss-logo" />
-            <p className="text-3xl text-center uppercase tracking-wider font-poppins font-semibold">National Service Scheme</p>
-            <p className="text-2xl tracking-wide font-poppins -mt-6">Programme Officers</p>
+          <div className="w-full lg:w-1/2 flex flex-col items-center gap-12 text-center">
+            <img className="w-44 lg:h-44" src="/assets/Schemes/nsslogo.png" alt="nss-logo" />
+            <p className="text-3xl uppercase tracking-wider font-poppins font-semibold">National Service Scheme</p>
+            <p className="text-2xl tracking-wide font-poppins -mt-8">Programme Officers</p>
           </div>
 
-          <div className="w-1/2 flex flex-col items-center gap-8">
+          <div className="w-full lg:w-1/2 flex flex-col items-center gap-8">
             {
-              nss.length > 0 ? (
+              nss?.length > 0 ? (
                 nss?.map((n) => {
                   return (
-                    <div className="w-full flex justify-center">
-                      <div className="flex flex-col gap-2 items-center">
-                        <div className="flex flex-row items-center gap-4">
-                          <p className="text-2xl tracking-wide font-semibold font-poppins">{n.name}</p>
-                          <p className="text-7xl leading-3 -mt-2 text-gray-700">·</p>
-                          <p className="text-xl font-poppins text-gray-700">{n.dept}</p>
-                        </div>
-                        {/* <div className="h-0.5 w-[30%] bg-gray-400"></div> */}
-                      </div>
+                    <div className="flex flex-col lg:flex-row items-center gap-1 lg:gap-4 text-center">
+                      <p className="text-2xl tracking-wide font-semibold font-poppins">{n.name}</p>
+                      <p className="hidden lg:block text-7xl leading-3 -mt-2 text-gray-700">·</p>
+                      <p className="text-xl font-poppins text-gray-700">{n.dept}</p>
                     </div>
                   )
                 })
               ) : (
-                <div className="w-full flex flex-col items-center gap-4">
+                <div className="w-full flex flex-col items-center gap-8">
                   {[...Array(4)].map((i) => (
-                    <div className="w-full flex flex-row items-center justify-center gap-8">
-                      <div className="w-1/3 h-12 rounded-lg" id="skeleton"></div>
-                      <div className="w-1/3 h-8 rounded-md" id="skeleton"></div>
+                    <div className="w-full flex flex-col lg:flex-row items-center justify-center gap-4 lg:gap-8">
+                      <div className="w-3/4 lg:w-1/3 h-12 rounded-lg" id="skeleton"></div>
+                      <div className="w-1/2 lg:w-1/3 h-8 rounded-md" id="skeleton"></div>
                     </div>
                   ))}
                 </div>
@@ -60,38 +55,35 @@ const SchemesWings = () => {
           </div>
         </div>
 
-        <div className="w-full bg-gray-200 rounded-2xl flex flex-row py-12 items-center shadow-md">
+        <div className="w-full bg-gray-200 rounded-2xl flex flex-col lg:flex-row py-12 px-6 items-center shadow-md gap-12 lg:gap-6">
 
-          <div className="w-1/2 flex flex-col items-center gap-12">
+          <div className="w-full lg:w-1/2 flex flex-col items-center gap-12 text-center">
             <img className="h-44" src="/assets/Schemes/ncclogo.png" alt="ncc-logo" />
-            <p className="text-3xl text-center uppercase tracking-wider font-poppins font-semibold">National Cadet Corps</p>
-            <p className="text-2xl tracking-wide font-poppins -mt-6">NCC Officers</p>
+            <p className="text-3xl uppercase tracking-wider font-poppins font-semibold">National Cadet Corps</p>
+            <p className="text-2xl tracking-wide font-poppins -mt-8">NCC Officers</p>
           </div>
 
-          <div className="w-1/2 flex flex-col items-center gap-8">
+          <div className="w-full lg:w-1/2 flex flex-col items-center gap-10">
             {
-              ncc.length > 0 ? (
+              ncc?.length > 0 ? (
                 ncc?.map((n) => {
                   return (
-                    <div className="w-full flex justify-center">
-                      <div className="flex flex-col gap-2 items-center">
-                        <div className="flex flex-row items-center gap-4">
-                          <p className="text-2xl tracking-wide font-semibold font-poppins">{n.name}</p>
-                          <p className="text-7xl leading-3 -mt-2 text-gray-700">·</p>
-                          <p className="text-xl font-poppins text-gray-700">{n.dept}</p>
-                        </div>
-                        <p className="text-l font-poppins text-gray-700">{n.role}</p>
-                        {/* <div className="h-0.5 w-[30%] bg-gray-400"></div> */}
+                    <div className="flex flex-col gap-1 items-center text-center">
+                      <div className="flex flex-col lg:flex-row items-center gap-1 lg:gap-4">
+                        <p className="text-2xl tracking-wide font-semibold font-poppins">{n.name}</p>
+                        <p className="hidden lg:block text-7xl leading-3 -mt-2 text-gray-700">·</p>
+                        <p className="text-xl font-poppins text-gray-700">{n.dept}</p>
                       </div>
+                      <p className="font-poppins text-gray-700">{n.role}</p>
                     </div>
                   )
                 })
               ) : (
-                <div className="w-full flex flex-col items-center gap-4">
+                <div className="w-full flex flex-col items-center gap-8">
                   {[...Array(4)].map((i) => (
-                    <div className="w-full flex flex-row items-center justify-center gap-8">
-                      <div className="w-1/3 h-12 rounded-lg" id="skeleton"></div>
-                      <div className="w-1/3 h-8 rounded-md" id="skeleton"></div>
+                    <div className="w-full flex flex-col lg:flex-row items-center justify-center gap-4 lg:gap-8">
+                      <div className="w-3/4 lg:w-1/3 h-12 rounded-lg" id="skeleton"></div>
+                      <div className="w-1/2 lg:w-1/3 h-8 rounded-md" id="skeleton"></div>
                     </div>
                   ))}
                 </div>
